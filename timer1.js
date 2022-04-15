58b5f046546f88c440e7dcf61ca961cc4cdebdce
Cesar Santamaria <cesar-santamaria@outlook.com>
2022-04-15 09:09:58 -0500

    refactor code and account for edge cases

--- timer1.js
+++ timer1.js
@@ -1,11 +1,22 @@
-const alarm = (wakupTimes) => {
+const alarm = () => {
   const args = process.argv.slice(2); //needed in order to accept arguments from terminal.
-  let delay = args;
+  let alarmTimes = args;
 
-  for(i = 0; i < args.length; i++){
-    setTimeout(()=>{
+  if (alarmTimes.length === 0) console.log('No alarm times were provided');
+
+  // for loop to cycle through arguments set in command line which are then
+  for (let i = 0; i < args.length; i++) {
+    const individualAlarmTimes = parseInt(alarmTimes[i]);
+  
+    if (individualAlarmTimes < 0 || isNaN(individualAlarmTimes) === true) {
+      console.log(`Please enter a valid alarm time`);
+      process.exit();
+    }
+
+    setTimeout(()=> {
       process.stdout.write('\x07');
-    },delay[i] * 1000)
+      console.log(`Alarm set at ${individualAlarmTimes} second`);
+    },individualAlarmTimes * 1000);
   }
 };
 
